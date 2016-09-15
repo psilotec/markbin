da9d92f48b06e3f858dc5cb362f39b8419e386bd
psilotec <scottadevito@gmail.com>
2016-09-15 18:30:06 -0400

    Create bins.insert Method

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -3,4 +3,15 @@
  */
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+    'bins.insert': function() {
+        return Bins.insert({
+            createdAt: new Date(),
+            content: '',
+            sharedWith: [],
+            ownerId: this.userId
+        });
+    }
+});
+
 export const Bins = new Mongo.Collection('bins');
