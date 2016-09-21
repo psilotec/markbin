85183ee4580766192e60e8b522ce44d0071a0444
psilotec <scottadevito@gmail.com>
2016-09-20 21:32:43 -0400

    Create BinsMain Component, Route, and Render to App

---
+++ client/components/bins/bins_main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Scott on 9/20/2016.
+ */
+import React, { Component } from 'react';
+
+class BinsMain extends Component {
+    render() {
+        return (
+            <div>BinsMain</div>
+        );
+    }
+}
+
+export default BinsMain;
